3ddb951cca7ac5f3012ab6908c05925cae461747
udinocera <udinocera@gmail.com>
2022-04-07 19:32:02 +0200

    Fine esercizio

--- Geometria/Program.cs
+++ Geometria/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using geometria;
+
+
+
+Console.Write("Inserisci un numero per creare la base del rettangolo ");
+int baseUtente = int.Parse(Console.ReadLine());
+
+Console.Write("Inserisci un numero per creare l'altezza del rettangolo ");
+int altezzaUtente = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+Rettangolo MioRettangolo = new Rettangolo(baseUtente, altezzaUtente);
+
+Console.WriteLine($"---------- Rettangolo1 ----------");
+
+Console.WriteLine($"Il perimetro del rettangolo è {MioRettangolo.Perimetro(baseUtente, altezzaUtente)}");
+Console.WriteLine($"L'area del rettangolo è {MioRettangolo.Area(baseUtente, altezzaUtente)}");
+Console.WriteLine($"---------------------------------");
+
+
+
+
+Console.Write("Inserisci un numero per creare la base del secondo rettangolo ");
+int baseUtenteSecondo = int.Parse(Console.ReadLine());
+
+Console.Write("Inserisci un numero per creare l'altezza del secondo rettangolo ");
+int altezzaUtenteSecondo = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+RettangoloSecondo secondoMioRettangolo = new RettangoloSecondo(baseUtenteSecondo, altezzaUtenteSecondo);
+
+Console.WriteLine($"---------- Rettangolo2 ----------");
+
+Console.WriteLine($"Il perimetro del rettangolo è {secondoMioRettangolo.perimetroSecondo(baseUtente, altezzaUtente)}");
+Console.WriteLine($"L'area del rettangolo è {secondoMioRettangolo.areaSecondo(baseUtenteSecondo, altezzaUtenteSecondo)}");
+Console.WriteLine($"---------------------------------");
---
+++ Geometria/rettangolo.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geometria
+{
+    public class Rettangolo
+    {
+        public int baseRettangolo;
+        public int altezzaRettangolo;
+
+        public Rettangolo(int baseRettangolo, int altezzaRettangolo)
+        {
+            this.baseRettangolo = baseRettangolo;
+            this.altezzaRettangolo = altezzaRettangolo;
+        }
+        // PERIMETRO RETTANGOLO
+        public int Perimetro(int baseRettangolo, int altezzaRettangolo)
+        {
+            this.baseRettangolo = baseRettangolo;
+            this.altezzaRettangolo = altezzaRettangolo;
+            int perimetoRettangolo = 2 * (baseRettangolo + altezzaRettangolo);
+            return perimetoRettangolo;
+
+        }
+
+        // AREA RETTANGOLO
+        public int Area(int baseRettangolo, int altezzaRettangolo)
+        {
+            this.baseRettangolo = baseRettangolo;
+            this.altezzaRettangolo = altezzaRettangolo;
+            int areaRettangolo = baseRettangolo * altezzaRettangolo;
+            return areaRettangolo;
+        }
+    }
+
+    public class RettangoloSecondo
+    {
+        public int BaseRettangoloSecondo;
+        public int AltezzaRettangoloSecondo;
+
+        public RettangoloSecondo(int BaseRettangoloSecondo, int AltezzaRettangoloSecondo)
+        {
+            this.BaseRettangoloSecondo = BaseRettangoloSecondo;
+            this.AltezzaRettangoloSecondo = AltezzaRettangoloSecondo;
+        }
+        // PERIMETRO RETTANGOLO
+        public int perimetroSecondo(int BaseRettangoloSecondo, int AltezzaRettangoloSecondo)
+        {
+            this.BaseRettangoloSecondo = BaseRettangoloSecondo;
+            this.AltezzaRettangoloSecondo = AltezzaRettangoloSecondo;
+            int PerimetroRettangoloSecondo = 2 * (BaseRettangoloSecondo + AltezzaRettangoloSecondo);
+            return PerimetroRettangoloSecondo;
+
+        }
+
+        // AREA RETTANGOLO
+        public int areaSecondo(int BaseRettangoloSecondo, int AltezzaRettangoloSecondo)
+        {
+            this.BaseRettangoloSecondo = BaseRettangoloSecondo;
+            this.AltezzaRettangoloSecondo = AltezzaRettangoloSecondo;
+            int AreaRettangoloSecondo = BaseRettangoloSecondo * AltezzaRettangoloSecondo;
+            return AreaRettangoloSecondo;
+        }
+    }
+
+}
