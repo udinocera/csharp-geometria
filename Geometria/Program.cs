ad2d03b281af83e8f27c74bb3a19b03d5e6b4184
udinocera <udinocera@gmail.com>
2022-04-07 19:33:05 +0200

    Fine Rettangolo

--- Geometria/Program.cs
+++ Geometria/Program.cs
@@ -1,7 +1,6 @@
-﻿
+﻿// See https://aka.ms/new-console-template for more information
 
 using geometria;
-
 
 
 Console.Write("Inserisci un numero per creare la base del rettangolo ");
@@ -14,11 +13,12 @@ Console.WriteLine();
 
 Rettangolo MioRettangolo = new Rettangolo(baseUtente, altezzaUtente);
 
-Console.WriteLine($"---------- Rettangolo1 ----------");
-
-Console.WriteLine($"Il perimetro del rettangolo è {MioRettangolo.Perimetro(baseUtente, altezzaUtente)}");
-Console.WriteLine($"L'area del rettangolo è {MioRettangolo.Area(baseUtente, altezzaUtente)}");
-Console.WriteLine($"---------------------------------");
+Console.WriteLine($"---------- Rettangolo1 ----------------");
+Console.WriteLine($"La base del rettangolo è {(baseUtente)} cm");
+Console.WriteLine($"L'altezza del rettangolo è {(altezzaUtente)} cm");
+Console.WriteLine($"Il perimetro del rettangolo è {MioRettangolo.Perimetro(baseUtente, altezzaUtente)} cm");
+Console.WriteLine($"L'area del rettangolo è {MioRettangolo.Area(baseUtente, altezzaUtente)} cm^2 ");
+Console.WriteLine($"---------------------------------------");
 
 
 
@@ -33,8 +33,9 @@ Console.WriteLine();
 
 RettangoloSecondo secondoMioRettangolo = new RettangoloSecondo(baseUtenteSecondo, altezzaUtenteSecondo);
 
-Console.WriteLine($"---------- Rettangolo2 ----------");
-
-Console.WriteLine($"Il perimetro del rettangolo è {secondoMioRettangolo.perimetroSecondo(baseUtente, altezzaUtente)}");
-Console.WriteLine($"L'area del rettangolo è {secondoMioRettangolo.areaSecondo(baseUtenteSecondo, altezzaUtenteSecondo)}");
-Console.WriteLine($"---------------------------------");
+Console.WriteLine($"---------- Rettangolo2 -----------------");
+Console.WriteLine($"La base del rettangolo è {(baseUtenteSecondo)} cm");
+Console.WriteLine($"L'altezza del rettangolo è {(altezzaUtenteSecondo)} cm");
+Console.WriteLine($"Il perimetro del rettangolo è {secondoMioRettangolo.perimetroSecondo(baseUtente, altezzaUtente)} cm");
+Console.WriteLine($"L'area del rettangolo è {secondoMioRettangolo.areaSecondo(baseUtenteSecondo, altezzaUtenteSecondo)} cm^2");
+Console.WriteLine($"----------------------------------------");
